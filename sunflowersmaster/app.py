3c404f7c3014e5bd9b469204910c2fe594315d50
masterSunflowers <lvdthieu@gmail.com>
2024-09-27 14:04:32 +0700

    Update server side

--- sunflowersmaster/app.py
+++ sunflowersmaster/app.py
@@ -1,15 +1,14 @@
 import gzip
 import json
+import logging
 import os
 import signal
-import sqlite3
 import sys
 import tarfile
-from collections import defaultdict
 from io import BytesIO
 from pathlib import Path
 from time import sleep
-from typing import Tuple
+from typing import List, Tuple
 
 import docker
 import docker.errors
@@ -24,6 +23,11 @@ dotenv.load_dotenv(override=True)
 app = Flask(__name__)
 
 WORK_DIR = os.path.abspath(os.path.dirname(__file__))
+logger = logging.Logger("master")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 
 def handle_termination(signal, frame):
@@ -32,23 +36,16 @@ def handle_termination(signal, frame):
             machines = json.load(f)
     except Exception as e:
         machines = {}
-    for machine_id in machines:
-        try:
-            container = client.containers.get(
-                f"{machine_id}--{machines[machine_id]['session_id']}"
-            )
-            if container.status == "running":
-                container.stop()
-            container.remove()
-        except docker.errors.NotFound:
-            print(
-                f"Container name {machine_id}--{machines[machine_id]['session_id']} not found"
-            )
-        except Exception as e:
-            print(f"An error occur: {e}")
+    try:
+        for machine_id in machines:
+            for session_id in machines[machine_id]:
+                kill_worker(machine_id, session_id)
 
-    with open(os.path.join(WORK_DIR, "machines.json"), "w") as f:
-        json.dump({}, f)
+        with open(os.path.join(WORK_DIR, "machines.json"), "w") as f:
+            json.dump({}, f)
+    except Exception as e:
+        logger.error("Flask server is not killed gracefully")
+        logger.error(f"Encounter error: {e}")
     sys.exit(0)
 
 
@@ -56,97 +53,105 @@ signal.signal(signalnum=signal.SIGINT, handler=handle_termination)
 signal.signal(signalnum=signal.SIGTERM, handler=handle_termination)
 
 
-@app.post("/v1/api/store")
-def store_project():
-    try:
-        machine_id = request.args.get("machineId")
-        session_id = request.args.get("sessionId")
-        compressed_data = request.data
-        decompressed_data = gzip.decompress(compressed_data)
-        data = json.loads(decompressed_data)
-        if not data:
-            return {"message": f"No data received"}, 200
-        workspace = os.path.split(data[0]["filePath"])[0]
-        if os.path.exists(os.path.join("projects", machine_id, workspace)):
-            os.system(
-                f"rm -rf {os.path.join('projects', machine_id, workspace)}"
-            )
-        for item in data:
-            absolute_path = Path(
-                os.path.join("projects", machine_id, item["filePath"])
-            )
-            os.makedirs(absolute_path.parent, exist_ok=True)
-            with open(
-                absolute_path, "w", encoding="utf-8", errors="ignore"
-            ) as f:
-                f.write(item["content"])
-        print("Received project!")
-        try:
-            with open(os.path.join(WORK_DIR, "machines.json"), "r") as f:
-                machines = json.load(f)
-        except Exception:
-            machines = {}
-        machines[machine_id] = {}
-        machines[machine_id]["session_id"] = session_id
-        machines[machine_id]["workspace"] = workspace
-        with open(os.path.join(WORK_DIR, "machines.json"), "w") as f:
-            json.dump(machines, f)
-        return {"message": f"Data received successfully"}, 200
-    except Exception as e:
-        print(e)
-        return {"error": str(e)}, 500
-
-
-def rename_worker(
-    machine_id: str, old_session_id: str, new_session_id: str
-) -> int:
-    try:
-        container = client.containers.get(f"{machine_id}--{old_session_id}")
-        container.rename(f"{machine_id}--{new_session_id}")
-        print(
-            (
-                f"Rename worker:\n"
-                f"\tfrom: {machine_id}--{old_session_id}\n"
-                f"\tto: {machine_id}--{new_session_id}"
-            )
+def kill_worker(machine_id: str, session_id: str):
+    machines = get_list_machines()
+    if not machines.get(machine_id, []):
+        machines[machine_id] = []
+        sessions = machines[machine_id]
+    else:
+        sessions = machines[machine_id]
+    if session_id not in sessions:
+        logger.info(
+            f"Worker {machine_id}--{session_id} has not been created yet"
         )
-        return 0
-    except:
-        print("Failed to rename worker")
-        return 1
+        return
+    try:
+        worker = client.containers.get(f"{machine_id}--{session_id}")
+        # if worker.status == "running":
+        #     worker.kill()
+        worker.remove(force=True)
+        logger.info(f"Killed worker named {machine_id}--{session_id}")
+        sessions.remove(session_id)
+        store_list_machines(machines)
+    except docker.errors.NotFound:
+        logger.error(f"Worker named {machine_id}--{session_id} not found")
+        raise Exception
+    except Exception as e:
+        logger.error(
+            f"An error occur while killing worker named {machine_id}--{session_id}: {e}"
+        )
+        raise Exception
+
+
+def get_list_machines() -> Tuple[str, List[str]]:
+    try:
+        with open(os.path.join(WORK_DIR, "machines.json"), "r") as f:
+            machines = json.load(f)
+    except Exception as e:
+        machines = {}
+    return machines
+
+
+def store_list_machines(machines: Tuple[str, List[str]]):
+    with open(os.path.join(WORK_DIR, "machines.json"), "w") as f:
+        json.dump(machines, f)
 
 
 def create_worker(machine_id: str, session_id: str) -> int:
+    machines = get_list_machines()
+    if not machines.get(machine_id, []):
+        machines[machine_id] = []
+        sessions = machines[machine_id]
+    else:
+        sessions = machines[machine_id]
+    logger.debug("Machines info:")
+    logger.debug(machines)
+    if session_id in sessions:
+        logger.info("Worker existed")
+        return 0
     try:
         client.containers.run(
             "worker",
             detach=True,
             name=f"{machine_id}--{session_id}",
-            ports={"8001/tcp": 8001},
+            # ports={"8001/tcp": 8001},
+            network="host",
         )
-        container = client.containers.get(f"{machine_id}--{session_id}")
+        worker = client.containers.get(f"{machine_id}--{session_id}")
+        if worker:
+            logger.info("Worker is created")
+            sessions.append(session_id)
+            logger.debug(sessions)
+            logger.debug(machines)
+            store_list_machines(machines)
+
         message = "* Running on all addresses (0.0.0.0)"
         timeout = int(os.environ.get("INIT_WORKER_TIMEOUT"))
         stop_time = 1
         elapsed_time = 0
-        while (
-            message not in container.logs().decode() and elapsed_time < timeout
-        ):
+        while message not in worker.logs().decode() and elapsed_time < timeout:
             sleep(stop_time)
             elapsed_time += stop_time
             continue
-        if message in container.logs().decode():
-            print("Container is ready!")
+
+        if message in worker.logs().decode():
+            logger.info("Worker is ready!")
             return 0
         else:
-            print("Container take too long to start!")
+            logger.error("Worker take too long to start!")
+            try:
+                kill_worker(machine_id, session_id)
+            except Exception as e:
+                logger.error(
+                    f"Worker named {machine_id}--{session_id} is created but not running yet"
+                )
             return 2
     except Exception as e:
-        print(e)
+        logger.error(f"Creat worker error: {e}")
         return 1
 
 
-def tar(folder_path):
+def tar(folder_path: str) -> BytesIO:
     tar_stream = BytesIO()
     with tarfile.open(fileobj=tar_stream, mode="w") as tar:
         for item in os.listdir(folder_path):
@@ -156,95 +161,116 @@ def tar(folder_path):
     return tar_stream
 
 
-def copy_to_container(
-    container_name: str, folder_path: str, container_dest: str
-):
+def copy_to_worker(worker_name: str, folder_path: str, worker_dest: str):
     try:
-        container = client.containers.get(container_name)
+        worker = client.containers.get(worker_name)
         tar_stream = tar(folder_path)
-        print(
-            f"Copying contents of {folder_path} to container {container_name}:{container_dest}"
+        logger.info(
+            f"Copying contents of {folder_path} to worker {worker_name}:{worker_dest}"
         )
-        container.put_archive(container_dest, tar_stream)
-        print("Copied!")
+        worker.put_archive(worker_dest, tar_stream)
+        logger.info("Copied!")
     except docker.errors.NotFound:
-        print(f"Container {container_name} not found.")
+        logger.error(f"Worker {worker_name} not found.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.error(f"Copy to worker error: {e}")
 
 
-# DOING
-def run_worker(machine_id: str, advanced: bool, data) -> Tuple[str, str]:
-    with open(os.path.join(WORK_DIR, "machines.json"), "r") as f:
-        machines = json.load(f)
-
-    session_id = machines[machine_id]["session_id"]
-    workspace = machines[machine_id].get("workspace", None)
+# TODO
+def run_worker(
+    machine_id: str, session_id: str, advanced: bool, data
+) -> Tuple[str, str]:
     if advanced == "false":
-        print("Generating normal mode")
+        logger.info("Generating normal mode")
         endpoint = "http://localhost:8001/v1/api/normal"
     else:
-        print("Generating advanced mode")
-        container_name = f"{machine_id}--{session_id}"
-        folder_path = os.path.join(WORK_DIR, "projects", machine_id, workspace)
-        container_dest = "/workspace/project"
-        copy_to_container(container_name, folder_path, container_dest)
+        logger.info("Generating advanced mode")
+        worker_name = f"{machine_id}--{session_id}"
+        folder_path = os.path.join(WORK_DIR, "projects", machine_id, session_id)
+        worker_dest = "/workspace/project"
+        copy_to_worker(worker_name, folder_path, worker_dest)
         endpoint = "http://localhost:8001/v1/api/advanced"
 
-    print("Request body:")
-    print(data.keys())
+    logger.info("Sending request to worker")
     response = requests.post(
         url=endpoint,
         json=data,
         headers={"HTTP_ACCEPT": "application/json"},
         params={"machineId": machine_id, "sessionId": session_id},
     )
-    print("Sent request")
+    logger.info("Received response from worker")
     if response.status_code == 200:
         return response.json()["code"], None
     else:
-        print(f"Container message: {response.json()['error']}")
+        logger.error(f"Worker message: {response.json()['error']}")
         raise Exception(response.json()["error"])
+
+
+@app.post("/v1/api/store")
+def store_project():
+    logger.info("Received store project request")
+    try:
+        machine_id = request.args.get("machineId")
+        session_id = request.args.get("sessionId")
+        compressed_data = request.data
+        decompressed_data = gzip.decompress(compressed_data)
+        data = json.loads(decompressed_data)
+        if not data:
+            return {"message": f"No data received"}, 200
+        if os.path.exists(os.path.join("projects", machine_id, session_id)):
+            os.system(
+                f"rm -rf {os.path.join('projects', machine_id, session_id)}"
+            )
+        for item in data:
+            absolute_path = Path(
+                os.path.join(
+                    "projects", machine_id, session_id, item["filePath"]
+                )
+            )
+            os.makedirs(absolute_path.parent, exist_ok=True)
+            with open(
+                absolute_path, "w", encoding="utf-8", errors="ignore"
+            ) as f:
+                f.write(item["content"])
+        logger.info("Received project!")
+        return {"message": f"Data received successfully"}, 200
+    except Exception as e:
+        logger.error(f"Store project error: {e}")
+        return {"error": str(e)}, 500
+
+
+@app.delete("/v1/api/kill-session")
+def kill_session():
+    logger.info("Received kill session request")
+    machine_id = request.args.get("machineId")
+    session_id = request.args.get("sessionId")
+    try:
+        kill_worker(machine_id, session_id)
+        logger.info("Killed session succesfully")
+        return {"message": "Kill session success"}, 200
+    except Exception as e:
+        logger.error("Can not kill session")
+        logger.error(f"Encounter error: {e}")
+        return {"message": "Kill session error"}, 500
 
 
 @app.post("/v1/api/gen")
 def gen():
-    print("Received gen request")
+    logger.info("Received generate request")
     try:
         machine_id = request.args.get("machineId")
         session_id = request.args.get("sessionId")
         advanced = request.args.get("advanced")
         data = json.loads(request.data)
-        try:
-            with open(os.path.join(WORK_DIR, "machines.json"), "r") as f:
-                machines = json.load(f)
-
-        except Exception as e:
-            machines = {}
-        if not machines.get(machine_id, None):
-            return_code = create_worker(machine_id, session_id)
-            if return_code == 1:
-                raise Exception("Can not create worker")
-            machines[machine_id] = {}
-            machines[machine_id]["session_id"] = session_id
-            with open(os.path.join(WORK_DIR, "machines.json"), "w") as f:
-                json.dump(machines, f)
-        else:
-            if machines[machine_id]["session_id"] != session_id:
-                return_code = rename_worker(
-                    machine_id, machines[machine_id]["session_id"], session_id
-                )
-                if return_code == 1:
-                    raise Exception("Can not rename worker")
-                machines[machine_id]["session_id"] = session_id
-                with open(os.path.join(WORK_DIR, "machines.json"), "w") as f:
-                    json.dump(machines, f)
-        print("Prepare to run worker")
-        code, details = run_worker(machine_id, advanced, data)
-        print("Generate successfully")
+        return_code = create_worker(machine_id, session_id)
+        if return_code != 0:
+            raise Exception("Can not create worker")
+        logger.debug("Prepare to run worker")
+        code, details = run_worker(machine_id, session_id, advanced, data)
+        logger.debug("Generate successfully")
         return {"code": f"```python\n{code}\n```", "details": details}, 200
     except Exception as e:
-        print(e)
+        logger.error(f"Generate error: {e}")
         if "authentication_error" in str(e):
             return {"error": str(e)}, 401
         else:
--- sunflowersworker/app.py
+++ sunflowersworker/app.py
@@ -1,9 +1,12 @@
 import json
 import logging
 import os
+import subprocess
 import sys
+import time
+from typing import Dict, List, Tuple
 
-import sonarqube
+import requests
 from flask import Flask, request
 from openai import OpenAI
 
@@ -16,11 +19,9 @@ handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 WORK_DIR = os.path.abspath(os.path.dirname(__file__))
-
-sonar = sonarqube.SonarCloudClient(
-    sonarqube_url="http://localhost:9000",
-    token="squ_aaefe98d8c3aa5d713b685b60052be316f281e64",
-)
+SONAR_TOKEN = "squ_eaaf984a9814658d689ffecff829d4fd136fe4b0"
+SONAR_SOURCES = "/workspace/project/"
+SONAR_PROJECT_BASE_DIR = "/workspace/project/"
 
 
 def generate_code(
@@ -31,7 +32,7 @@ def generate_code(
     additional_context: str = None,
 ):
     final_prompt = (
-        "**Requirement**: Complete the function, return ONLY function body\n",
+        "**Requirement**: Complete the function\n",
         "\n"
         "**Code**:\n"
         f"{context}\n"
@@ -46,28 +47,54 @@ def generate_code(
             api_key=api_key,
             base_url=base_url,
         )
-
         messages = [
             {"role": "system", "content": "You are a professional developer."},
             {"role": "user", "content": str(final_prompt)},
             {"role": "assistant", "content": "```python\n", "prefix": True},
         ]
         response = client.chat.completions.create(
-            model="deepseek-chat",
+            model="deepseek-coder",
             messages=messages,
             stop=["```"],
         )
     except Exception as e:
         raise e
-    return response.choices[0].message.content
+    # return response.choices[0].message.content, messages.append(
+    #     response.choices[0].message
+    # )
+    code = """
+def _unix_pattern_to_parameter_names(
+    constraints: List[str], all_parameter_names: Set[str]
+) -> Union[None, Set[str]]:
+    \"\"\"
+    Convert Unix-style patterns in constraints to a set of parameter names.
+
+    Args:
+        constraints (List[str]): List of Unix-style patterns to match against parameter names.
+        all_parameter_names (Set[str]): Set of all available parameter names.
+
+    Returns:
+        Union[None, Set[str]]: Set of parameter names that match the constraints, or None if no matches are found.
+    \"\"\"
+    matchedParameters = set()
+
+    for pattern in constraints:
+        for param_name in all_parameter_names:
+            if fnmatch.fnmatch(param_name, pattern):
+                matchedParameters.add(param_name)
+
+    return matchedParameters if matchedParameters else None"""
+    response.choices[0].message.content = code
+    messages.append(response.choices[0].message)
+    return code, messages
 
 
 @app.post("/v1/api/normal")
 def normal_gen():
     try:
+        logger.info("Have received normal generate request!")
         data = json.loads(request.data)
-        logger.info(type(data))
-        code = generate_code(
+        code, _ = generate_code(
             data["prompt"], data["context"], data["baseUrl"], data["apiKey"]
         )
         logger.info("Generate successfully")
@@ -84,9 +111,8 @@ def advanced_gen():
         data = json.loads(request.data)
         machine_id = request.args.get("machineId")
         session_id = request.args.get("sessionId")
-        workspace_name = request.args.get("workspaceName")
         logger.info("Start to run pipeline")
-        code = run_pipeline(machine_id, session_id, workspace_name, data)
+        code = run_pipeline(machine_id, session_id, data)
         logger.info("Generate successfully")
         return {"code": code}, 200
     except Exception as e:
@@ -94,33 +120,138 @@ def advanced_gen():
         return {"error": str(e)}, 500
 
 
-
-# TODO
 def run_pipeline(machine_id, session_id, data):
-    initial_scan(machine_id, session_id)
-    for _ in range(data["maxIteration"]):
-        pass
-    return "So far so good" 
+    try:
+        logger.debug(f"Machine ID: {machine_id}")
+        logger.debug(f"Session ID: {session_id}")
+        initial_issues, num_initial_issue = get_issues(
+            f"{machine_id}--{session_id}", "1.0"
+        )
+        logger.debug(f"Num initial issue: {num_initial_issue}")
+        with open("initial_issues.json", "w") as f:
+            json.dump(initial_issues, f)
+    except Exception as e:
+        raise e
+    retrieved_context = retrieval(data["prompt"])
+    code, messages = generate_code(
+        data["prompt"],
+        data["context"],
+        data["baseUrl"],
+        data["apiKey"],
+        retrieved_context,
+    )
+    for i in range(1, data["maxIteration"]):
+        update_project(data["targetFile"], code)
+        logger.debug(f"Version: {i + 1}.0")
+        issues, num_issue = get_issues(
+            f"{machine_id}--{session_id}", f"{i + 1}.0"
+        )
+        logger.debug(f"Num issue at version {i + 1}.0: {num_issue}")
+        generated_code_issues = check_code_issue(issues)
+        logger.debug(f"Generated code issues: {generated_code_issues}")
+        if not generated_code_issues:
+            break
+        fixed_code, messages = fix_code(
+            messages, generated_code_issues, data["apiKey"], data["baseUrl"]
+        )
+        code = fixed_code
+    return code
 
 
-# TODO
-def check_linting():
-    pass
+def get_issues(project_key: str, version: str) -> Tuple[Dict, str]:
+    try:
+        task_status_url = scan(project_key, project_key, version)
+        if not task_status_url:
+            raise Exception("Can not find task_id")
+    except Exception as e:
+        logger.error(e)
+        raise Exception(f"Can not scan project {project_key}")
+    try:
+        logger.debug("Nice try")
+        response = requests.get(
+            url=task_status_url,
+            headers={"Authorization": f"Bearer {SONAR_TOKEN}"},
+        )
+        logger.debug(response.status_code)
+        response = response.json()
+        report_processing_status = response["task"]["status"]
+        logger.debug(f"Report processing status: {report_processing_status}")
+        timeout = 30
+        elapsed_time = 0
+        stop_time = 1
+        while report_processing_status != "SUCCESS" and elapsed_time < timeout:
+            time.sleep(stop_time)
+            elapsed_time += stop_time
+            response = requests.get(
+                url=task_status_url,
+                headers={"Authorization": f"Bearer {SONAR_TOKEN}"},
+            )
+            report_processing_status = response.json()["task"]["status"]
+            logger.debug(
+                f"Report processing status: {report_processing_status}"
+            )
+        if elapsed_time >= timeout:
+            raise TimeoutError("Report processing timeout")
+        page = 1
+        page_size = 100
+        response = requests.get(
+            url="http://localhost:9000/api/issues/search",
+            headers={"Authorization": f"Bearer {SONAR_TOKEN}"},
+            params={"p": page, "ps": page_size},
+        )
+        total = response.json()["total"]
+        issues = {}
+        while page * page_size - 100 < total:
+            response = requests.get(
+                url="http://localhost:9000/api/issues/search",
+                headers={"Authorization": f"Bearer {SONAR_TOKEN}"},
+                params={"p": page, "ps": page_size},
+            )
+            issues_in_page = {
+                issue["key"]: issue for issue in response.json()["issues"]
+            }
+            issues.update(issues_in_page)
+            page += 1
+        return issues, total
+    except Exception as e:
+        raise e
 
 
-# TODO
+def scan(project_key: str, project_name: str, version: str):
+    cmd = (
+        "sonar-scanner -X "
+        f'-D"sonar.token={SONAR_TOKEN}" '
+        f'-D"sonar.projectKey={project_key}" '
+        f'-D"sonar.projectName={project_name}" '
+        f'-D"sonar.projectVersion={version}" '
+        f'-D"sonar.sources={SONAR_SOURCES}" '
+        f'-D"sonar.projectBaseDir={SONAR_PROJECT_BASE_DIR}" '
+        '-D"sonar.scm.disabled=True"'
+    )
+    res = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    if res.returncode == 0:
+        logger.info("Scanned project")
+        output_lines = res.stdout.splitlines()
+        pattern = "http://localhost:9000/api/ce/task?id="
+        for line in output_lines:
+            if line.find(pattern) >= 0:
+                task_status_url = line[line.find(pattern) :]
+                logger.debug(task_status_url)
+                return task_status_url
+        return None
+    else:
+        raise Exception("\nSTDOUT:\n" + res.stdout + "\nSTDERR\n" + res.stderr)
+
+
 def update_project(target_file: str, code: str):
-    absolute_path = os.path.join(WORK_DIR, target_file)
+    absolute_path = os.path.join(WORK_DIR, "project", target_file)
     with open(absolute_path, "r", encoding="utf-8", errors="ignore") as f:
         file_content = f.read()
-    new_content = file_content + "\n\n" + code
+    last_func_start_idx = file_content.rfind("def ")
+    new_content = file_content[:last_func_start_idx] + code
+    logger.debug(f"New content of target file {new_content}")
     with open(absolute_path, "w", encoding="utf-8", errors="ignore") as f:
         f.write(new_content)
-
-
-# TODO
-def store_result():
-    pass
 
 
 # TODO
@@ -128,5 +259,64 @@ def retrieval(prompt: str):
     return ""
 
 
+# TODO
+def check_code_issue(issues):
+    try:
+        with open("initial_issues.json", "r") as f:
+            initial_issues = json.load(f)
+
+        logger.debug(f"Num initial issues: {len(initial_issues)}")
+        logger.debug(f"Num issues: {len(issues)}")
+        new_issues = {
+            key: issue
+            for key, issue in issues.items()
+            if key not in initial_issues
+        }
+        message = "\n".join(
+            [new_issues[issue]["message"] for issue in new_issues]
+        )
+        logger.debug(message)
+        return message
+    except Exception as e:
+        logger.error(f"Check code issue {issues}")
+        raise e
+
+
+def fix_code(
+    messages: List, issue: str, api_key: str, base_url: str
+) -> Tuple[str, List]:
+    prev_answer = messages[-1]
+    code = prev_answer.content
+    new_command = (
+        "The code snippet:\n"
+        f"{code}\n"
+        "need to fix following these guildlines:\n"
+        f"{issue}\n"
+        "**Requirement**: Fix the code"
+    )
+    logger.debug(new_command)
+    messages = messages[:-2]
+    messages.append(prev_answer)
+    messages.append({"role": "user", "content": new_command})
+    messages.append(
+        {"role": "assistant", "content": "```python\n", "prefix": True}
+    )
+    logger.debug(messages)
+    try:
+        client = OpenAI(
+            api_key=api_key,
+            base_url=base_url,
+        )
+        response = client.chat.completions.create(
+            model="deepseek-coder",
+            messages=messages,
+            stop=["```"],
+        )
+        messages.append(response.choices[0].message)
+        return response.choices[0].message.content, messages
+    except Exception as e:
+        raise e
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=False)
